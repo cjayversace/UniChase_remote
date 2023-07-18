dddfe0d81e788b70cb28e46507c4fe72b137b5f6
cjayversace <cjay270@icloud.com>
2023-07-17 21:05:08 -0700

    Initial Commit

---
+++ UniChase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UniChase
+//
+//  Created by CJ Seigler on 7/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UniChase/UniChaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniChaseApp.swift
+//  UniChase
+//
+//  Created by CJ Seigler on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct UniChaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
